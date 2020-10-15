0ba15870ed59d1bc35007783dd1128c00e84e3c4
Gerard Arall <gerard@endouble.com>
2020-10-15 15:01:28 +0200

    Livewire fix

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
-    return redirect('/');
-});
-
 /**
  * Google OAuth
  */
