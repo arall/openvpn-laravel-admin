44c364baccc2f632a21500820001bd029925160a
arall <gerard.arall@gmail.com>
2020-12-07 13:04:20 +0100

    Fix location sorting

--- resources/views/livewire/admin/users/list.blade.php
+++ resources/views/livewire/admin/users/list.blade.php
@@ -47,7 +47,7 @@
                 <x-table.sortable :direction="$sortField === 'id' ? $sortDirection : null">ID</x-table.sortable>
                 <x-table.sortable :direction="$sortField === 'name' ? $sortDirection : null">Name</x-table.sortable>
                 <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">Role</th>
-                <x-table.sortable :direction="$sortField === 'locatiom' ? $sortDirection : null">Location</x-table.sortable>
+                <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">Location</th>
                 <x-table.sortable :direction="$sortField === 'email' ? $sortDirection : null">Email</x-table.sortable>
                 <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">Status</th>
             </tr>
