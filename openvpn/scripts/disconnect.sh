03a0c81ad60ad31bccfff9481ce60f534633822f
arall <gerard.arall@gmail.com>
2020-12-16 11:06:25 +0100

    Typo

--- openvpn/scripts/disconnect.sh
+++ openvpn/scripts/disconnect.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-. /etc/openvpn/scripts/config.sh
-. /etc/openvpn/scripts/functions.sh
-
-name=$(echap "$common_name")
-
-mysql -h$HOST -P$PORT -u$USER -p$PASS $DB -e "UPDATE users SET is_online = 0 WHERE email = '$name'"
 
 php /path/to/artisan openvpn:connect $common_name $trusted_ip $trusted_port $ifconfig_local $ifconfig_pool_remote_ip $remote_port_1 $bytes_received $bytes_sent
