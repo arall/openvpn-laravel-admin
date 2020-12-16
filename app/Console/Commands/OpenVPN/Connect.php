59543cbc3feb05c743d6e3c86e61c14b1b5e91ca
arall <gerard.arall@gmail.com>
2020-12-16 11:29:42 +0100

    Typo

--- app/Console/Commands/OpenVPN/Connect.php
+++ app/Console/Commands/OpenVPN/Connect.php
@@ -28,8 +28,16 @@ class Connect extends Command
     {
         $user = User::where('email', $this->argument('user'))->firstOrFail();
 
+        $user->logs()->create([
+            'client_ip' => $this->argument('client_ip'),
+            'client_port' => $this->argument('client_port'),
+            'local_ip' => $this->argument('local_ip'),
+            'remote_ip' => $this->argument('remote_ip'),
+            'remote_port' => $this->argument('remote_port'),
+            'bytes_received' => $this->argument('bytes_received'),
+            'bytes_sent' => $this->argument('bytes_sent'),
+        ]);
 
-
-        $user->setOffline();
+        $user->setOnline();
     }
 }
