2c374592fcda98397cb2b6b13f6463cb405161a9
arall <gerard.arall@gmail.com>
2020-12-18 11:10:11 +0100

    Add API

--- openvpn/scripts_remote/connect.sh
+++ openvpn/scripts_remote/connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H "Authorization: Bearer UZ4POfwGCFuG8PWHusPO" --data "user=$common_name&trusted_ip=$trusted_ip&trusted_port=$trusted_port&remote_ip=$ifconfig_pool_remote_ip&remote_port=$remote_port_1&bytes_received=$bytes_received&bytes_sent=$bytes_sent" "http://vpn.local/api/connect"  2>/dev/null
+curl -X POST -H "Authorization: Bearer TOKEN" --data "user=$common_name&trusted_ip=$trusted_ip&trusted_port=$trusted_port&remote_ip=$ifconfig_pool_remote_ip&remote_port=$remote_port_1&bytes_received=$bytes_received&bytes_sent=$bytes_sent" "http://vpn.local/api/connect"  2>/dev/null
